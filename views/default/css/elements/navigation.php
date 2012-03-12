117dd825aea554f5ff9a948642509043080cd1cd
ManUtopiK <emmanuel.salomon@free.fr>
2012-03-12 16:36:46 +0100

    merge from updated file of elgg core

--- views/default/css/elements/navigation.php
+++ views/default/css/elements/navigation.php
@@ -538,6 +538,27 @@
 	SIDEBAR EXTRAS (rss, bookmark, etc)
 *************************************** */
 .elgg-menu-extras {
-	float: left;
-	margin: 20px 0 10px;
+	margin-bottom: 15px;
+}
+
+/* ***************************************
+	WIDGET MENU
+*************************************** */
+.elgg-menu-widget > li {
+	position: absolute;
+	top: 4px;
+	display: inline-block;
+	width: 18px;
+	height: 18px;
+	padding: 2px 2px 0 0;
+}
+
+.elgg-menu-widget > .elgg-menu-item-collapse {
+	left: 5px;
+}
+.elgg-menu-widget > .elgg-menu-item-delete {
+	right: 5px;
+}
+.elgg-menu-widget > .elgg-menu-item-settings {
+	right: 25px;
 }
