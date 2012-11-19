d94abcdb85ef265295b66216eed472196a0e5adf
ManUtopiK <emmanuel.salomon@free.fr>
2012-11-19 15:00:20 +0100

    Delete new blog on blog group_module

--- views/default/blog/group_module.php
+++ views/default/blog/group_module.php
@@ -32,16 +32,10 @@ if (!$content) {
 	$content = '<p>' . elgg_echo('blog:none') . '</p>';
 }
 
-$new_link = elgg_view('output/url', array(
-	'href' => "blog/add/$group->guid",
-	'text' => elgg_echo('blog:write'),
-	'is_trusted' => true,
-));
 
 echo elgg_view('groups/profile/module', array(
 	'title' => elgg_echo('blog:group'),
 	'content' => $content,
 	'all_link' => "blog/group/$group->guid/all",
-	'add_link' => $new_link,
 	'stats' => $count,
 ));
