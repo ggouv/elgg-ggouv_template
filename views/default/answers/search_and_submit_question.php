dc7f111a521082121dcc2e6007a0693cbf18ef7e
ManUtopiK <emmanuel.salomon@free.fr>
2013-08-11 16:58:32 +0200

    Fix for answer plugin

--- views/default/answers/search_and_submit_question.php
+++ views/default/answers/search_and_submit_question.php
@@ -4,7 +4,15 @@
  *
  */
 
-echo '<h3 class="mbs">' . elgg_echo('answers:search_and_submit') . '</h3>';
+$container = elgg_extract('owner', $vars);
+
+if ($container->canWriteToContainer()) {
+	$echo = elgg_echo('answers:search_and_submit');
+} else {
+	$echo = elgg_echo('answers:search');
+}
+
+echo '<h3 class="mbs">' . $echo . '</h3>';
 
 echo elgg_view('input/text140', array(
 	'name' => 'body',
