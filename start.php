af0eceee620a59b52a0cd2c264425651b88d35e2
ManUtopiK <emmanuel.salomon@free.fr>
2014-01-13 01:03:29 +0100

    Avatar handler

--- start.php
+++ start.php
@@ -534,6 +534,73 @@ TEXT;
 
 
 /**
+ * Provide handler to return avatar for user and group
+ * URLs take the form of
+ *
+ *  user avatar:      avatar/user/username
+ *  user avatar:      avatar/user/username?size=$size
+ *  user avatar:      avatar/user/username/$size
+ *
+ *  group avatar:      avatar/group/groupname
+ *  group avatar:      avatar/group/groupname?size=$size
+ *  group avatar:      avatar/group/groupname/$size
+ *
+ * @return forward to avatar file or 404
+ */
+function avatar_handler($page) {
+	if (!isset($page[0]) || !isset($page[1])) {
+		header("HTTP/1.1 404 Not Found");
+		exit;
+	}
+
+	if ($page[0] == 'user') {
+		$object = get_user_by_username($page[1]);
+	} else if ($page[0] == 'group'){
+		$object = search_group_by_title($page[1]);
+	} else {
+		header("HTTP/1.1 404 Not Found");
+		exit;
+	}
+
+	$icon_sizes = elgg_get_config('icon_sizes');
+
+	$size = get_input('size', 'small');
+	if (!in_array($size, array_keys($icon_sizes))) {
+		$size = 'small';
+	}
+	// override size
+	if ($page[2] && in_array($size, array_keys($icon_sizes))) $size = $page[2];
+
+	if ($object instanceof ElggUser) {
+		// check if user already has an icon
+		if (!$object->icontime) {
+			// avatars must be square
+			$size = $icon_sizes[$size]['w'];
+
+			$hash = md5($object->email);
+			$forward = "https://secure.gravatar.com/avatar/$hash.jpg?r=pg&d=identicon&s=$size";
+		} else {
+			$forward = profile_override_avatar_url('entity:icon:url', 'user', false, array(
+				'entity' => $object,
+				'size' => $size
+			));
+		}
+	} else if ($object instanceof ElggGroup) {
+		set_input('group_guid', $object->getGUID());
+		set_input('size', $size);
+		$forward = 'groups/icon.php';
+	} else {
+		header("HTTP/1.1 404 Not Found");
+		exit;
+	}
+
+	header("Content-type: image/jpeg");
+	forward($forward);
+}
+
+
+
+/**
  * Add the rss link to the extras when if needed
  *
  * @return void
